20830f13dd48e7bb1ca7255780e732cedc41b999
Atai <atai.ibraimov10@gmail.com>
2022-02-24 11:46:27 +0600

    init finished

--- 23-02/1/script.js
+++ 23-02/1/script.js
@@ -1,3 +1,7 @@
+/*Создайте объект заработных плат.
+    Назовите userSalaries.
+    Свойствами выступают: Ella, Sophia, Ellie
+Выведите на экран зарплату Ella, Sophia.*/
 let Salaries = {
     name1:"Ella",
     name2:"Solphia",
@@ -9,4 +13,26 @@ let Salaries = {
 console.log(Salaries.SalaryElla);
 console.log(Salaries.SalarySolphia);
 
+/*Создайте объект с ключами a, b и c значениями 5 , 6 и 10.
+Найдите сумму его элементов.*/
 
+let summ={
+    a:5,
+    b:6,
+    c:10,
+
+};
+console.log(summ.a+summ.b+summ.c)
+/*Сделайте рефакторинг кода, с помощью тернарного оператора.*/
+let data = false;
+let number = (data === true) ? 3 : 5;
+console.log(number);
+// Используя конструкцию if..else, напишите код, который будет спрашивать: «Кто такой В.В.ПУТИН?».
+// Если посетитель вводит «Президент», то выводить «Верно!», если что-то другое – выводить «Не знаете?».
+let ques = prompt('Кто такой В.В.ПУТИН?', '');
+if(ques == 'Президент'){
+    alert('Верно!');
+}
+else{
+    alert('Не знаете?');
+}
