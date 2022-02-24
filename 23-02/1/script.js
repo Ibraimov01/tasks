b84b7dec3fdcdae9318795607c8149d8912d56eb
Atai <atai.ibraimov10@gmail.com>
2022-02-24 09:53:26 +0600

    init start

---
+++ 23-02/1/script.js
@@ -0,0 +1,12 @@
+let Salaries = {
+    name1:"Ella",
+    name2:"Solphia",
+    name3:"Eliie",
+    SalaryElla:500,
+    SalarySolphia:600,
+
+}
+console.log(Salaries.SalaryElla);
+console.log(Salaries.SalarySolphia);
+
+
