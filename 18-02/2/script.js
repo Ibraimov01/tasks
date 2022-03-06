c0b291711d9d10c3aee33801e6e4619e7ed26347
Atai <atai.ibraimov10@gmail.com>
2022-03-06 18:35:00 +0600

    finished

--- 18-02/2/script.js
+++ 18-02/2/script.js
@@ -1,4 +1,5 @@
-/*************#1*************/
+/*
+/!*************#1*************!/
 
 let num = 10;
 num ++;
@@ -8,47 +9,23 @@ console.log(num);
 num +1;
 console.log(num);
 
-/*************#2*************/
+/!*************#2*************!/
 
 let hello = 'Привет,';
 let name = 'Атай!';
-let info = hello + name;
+let info = hello + ' '+name;
 alert(info);
 let age =25;
 let str='Мне';
 let str2='лет!'
-let info1 = str + age + str2;
-alert(info1);
+alert(str + ' ' + age +' '+ str2);
 
-/*************#3*************/
+/!*************#3*************!/
 
 let a=17;
 let b=10;
 let c=a-b;
 let d=7;
 let result=c+d;
-console.log(result);
-
-/*************#4*************/
-let num2 = 47;
-num2 += 7;
-console.log(num2);
-alert(num2);
-let num3 = 47
-num3 -= 18;
-console.log(num3);
-alert(num3);
-let num4 = 47;
-num4 *= 10;
-console.log(num4);
-alert(num4)
-let num5 = 47;
-num5 /= 15;
-console.log(num5);
-alert(num5);
-
-num1 =num+=12;
-num2 =num-= 14;
-num3 =num*= 5;
-num4 =num /= 7;
-alert(num1);alert(num2);alert(num3);alert(num4);
+console.log(result);*/
+true + false = 1
